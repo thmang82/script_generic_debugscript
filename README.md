9465ed0075584b052e5934ea751929a89ce9e332
ThMang <thmang82@gmail.com>
2025-02-08 10:41:56 +0100

    initial commit 0.0.1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Home Script 'Debugscript'
+
+This is a data source script for the WunderView home script worker.
+- Category: 'compute'
+- Ident:    'debugscript'
+- Provides: 'compute'
+
+For debugging purposes
+
---
+++ src/spec.ts
@@ -0,0 +1,31 @@
+import { DataSource } from '../../toolchain/types/spec/spec_source';
+
+export const specification: DataSource.Specification = {
+    category:  "generic",
+    id_ident:  "debugscript",
+    id_author: "thmang82",
+    // ---
+    provides: [ "compute" ],
+    // ---
+    version:   "0.0.1",
+    // ---
+    author_email: "",
+    translations: {
+        'en' : { 
+            name: "Debug Script",
+            description: "For debugging purposes only"
+        }
+    },
+    // ---
+    parameters: [
+    ],
+    notifications: [],
+    geo_relevance: { 
+        everywhere: true
+    },
+    data_fetch: {
+        // Note: setting data_fetch to undefined will disable automatic fetching! You have to take care for yourself then, e.g. by subscribing to visiblity changes via ctx.script.visSubscribe
+        interval_active_sec: 5 * 60, // Fetch data every  5 minutes in case at least one screen showing data from this source is in state 'active'
+        interval_idle_sec: 15 * 60   // Fetch data every 15 minutes in case at least one screen showing data from this source is in state '(active) idle'
+    }
+}
